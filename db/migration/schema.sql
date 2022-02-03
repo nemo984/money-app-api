423d8740f438280e7a8648271015d187ea5a6b28
Moe Thant Kyaw <moethantk@gmail.com>
2022-02-03 15:11:42 +0700

    setup

---
+++ db/migration/schema.sql
@@ -0,0 +1,92 @@
+CREATE TYPE "date_frequency" AS ENUM (
+  'day',
+  'week',
+  'month',
+  'year'
+);
+
+CREATE TYPE "notification_priority" AS ENUM (
+  'low',
+  'medium',
+  'high'
+);
+
+CREATE TABLE "users" (
+  "user_id" serial PRIMARY KEY,
+  "username" varchar UNIQUE NOT NULL,
+  "name" varchar(15),
+  "password" varchar NOT NULL,
+  "profile_url" varchar,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "categories" (
+  "category_id" serial PRIMARY KEY,
+  "name" varchar NOT NULL
+);
+
+CREATE TABLE "expenses" (
+  "expense_id" serial PRIMARY KEY,
+  "category_id" int NOT NULL,
+  "amount" double NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "frequency" date_frequency,
+  "note" varchar,
+  "user_id" int NOT NULL
+);
+
+CREATE TABLE "incomes" (
+  "income_id" serial PRIMARY KEY,
+  "income_type_id" varchar NOT NULL,
+  "description" varchar,
+  "amount" double NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "frequency" date_frequency,
+  "user_id" int NOT NULL
+);
+
+CREATE TABLE "incomes_types" (
+  "income_type_id" serial PRIMARY KEY,
+  "type" varchar NOT NULL
+);
+
+CREATE TABLE "budgets" (
+  "budget_id" serial PRIMARY KEY,
+  "category_id" int,
+  "percentage" double NOT NULL,
+  "start_date" timestamptz DEFAULT (now()),
+  "end_date" timestamptz,
+  "user_id" int NOT NULL
+);
+
+CREATE TABLE "notifications" (
+  "notification_id" serial PRIMARY KEY,
+  "user_id" int NOT NULL,
+  "description" text,
+  "type" varchar NOT NULL,
+  "priority" notification_priority DEFAULT 'average',
+  "read" boolean DEFAULT false,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+ALTER TABLE "expenses" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("category_id");
+
+ALTER TABLE "expenses" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "incomes" ADD FOREIGN KEY ("income_type_id") REFERENCES "incomes_types" ("income_type_id");
+
+ALTER TABLE "incomes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "budgets" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("category_id");
+
+ALTER TABLE "budgets" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "notifications" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+CREATE INDEX "Username" ON "users" ("username");
+
+CREATE INDEX "Date" ON "expenses" ("created_at");
+
+CREATE INDEX "Date" ON "incomes" ("created_at");
+
+CREATE INDEX "Date" ON "notifications" ("created_at");
